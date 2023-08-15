5388571542f4b2d3e9e1db553087223c9d091cec
unknown <janoncarlos@gmail.com>
2023-08-15 09:55:35 -0300

    calendar react

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,12 +1,44 @@
 
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import enUS from 'date-fns/locale/en-US'
+import { addHours, getDay, startOfWeek, parse, format } from 'date-fns'
+const locales = {
+  'en-US': enUS,
+}
 
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+const events = [{
+  title: 'cumple',
+  notes: 'llevar regalo',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Carlos'
+  }
+
+}]
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
-
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
   )
 }
