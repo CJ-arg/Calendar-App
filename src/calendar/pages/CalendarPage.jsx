f613fda9ebc7435b8890c2abcda3fd8ba3f28cf5
unknown <janoncarlos@gmail.com>
2023-08-15 11:07:41 -0300

    calendar react

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,7 +5,7 @@ import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import { addHours } from 'date-fns'
-import { localizer } from '../../helpers'
+import { getMessagesEs, localizer } from '../../helpers'
 
 const events = [{
   title: 'cumple FAMILIAR ',
@@ -20,15 +20,29 @@ const events = [{
 
 }]
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    console.log({ event, start, end, isSelected });
+    return { style }
+  }
   return (
     <>
       <Navbar />
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
       />
     </>
   )
