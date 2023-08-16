230efbb24cd6fd0781095adc0fec1007d19ef257
unknown <janoncarlos@gmail.com>
2023-08-16 16:58:27 -0300

    modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -28,9 +28,53 @@ export const CalendarModal = () => {
       isOpen={isOpen}
       onRequestClose={OnCloseModal}
       style={customStyles}>
-      <h1>Hola mundo </h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum et, error hic quos consequuntur soluta, adipisci repellendus iste, totam fugit eaque! Repudiandae totam officia vitae quam tempora, nam ut incidunt! </p>
+      <form className="container">
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <hr />
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+          />
+          <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-outline-primary btn-block"
+        >
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+
+      </form>
     </Modal>
   )
 }
