5f71f37f0309ac1ac6eb558b7bb7e709ac3d026f
unknown <janoncarlos@gmail.com>
2023-08-30 16:53:30 -0300

    name in toolbar

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -9,27 +9,20 @@ import {
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
   const { events, activeEvent } = useSelector((state) => state.calendar);
-
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent));
   };
 
   const startSavingEvent = async (calendarEvent) => {
     // TODO: llegar al backend
-
-    // Todo bien
     if (calendarEvent._id) {
-      // Actualizando
       dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
-      // Creando
       dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
     }
   };
 
   const startDeletingEvent = () => {
-    // Todo: Llegar al backend
-
     dispatch(onDeleteEvent());
   };
 
