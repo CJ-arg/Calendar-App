6cd8151232d8b9f2bcb8c6bfb548642cd3421782
unknown <janoncarlos@gmail.com>
2023-08-30 15:28:27 -0300

    interceptors

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -6,6 +6,13 @@ const { VITE_API_URL } = getEnvVariables();
 const calendarApi = axios.create({
   baseURL: VITE_API_URL,
 });
-// TODO config interceptors
+calendarApi.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    "x-token": localStorage.getItem("token"),
+  };
+
+  return config;
+});
 
 export default calendarApi;
